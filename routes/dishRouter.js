314d8260bf8a14f1beeb3a4e52198a1994ed2088
Alexandr Belonogov <sanya_renin@mail.ru>
2019-02-19 17:26:34 +0500

    REST API

--- routes/dishRouter.js
+++ routes/dishRouter.js
@@ -25,4 +25,24 @@ dishRouter.route('/')
 		res.end('Deleting all dishes');
 	});
 
+dishRouter.route('/:id')
+	.all((req,res,next) => {
+		res.statusCode = 200;
+		res.setHeader('Content-Type', 'text/plain');
+		next();
+	})
+	.get((req,res,next) => {
+		res.end('Will send all the dishes to you! ' + req.params.id);
+	})
+	.post((req, res, next) => {
+		res.end('Will add the dish: ' + req.body.name + ' with details: ' + req.body.description);
+	})
+	.put((req, res, next) => {
+		res.statusCode = 403;
+		res.end('PUT operation not supported on /dishes');
+	})
+	.delete((req, res, next) => {
+		res.end('Deleting all dishes');
+	});
+
 module.exports = dishRouter;
