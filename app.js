c78d33d3294a3c13268dd22b397c931ab650264b
Alexandr Belonogov <sanya_renin@mail.ru>
2019-03-06 14:49:02 +0500

    Passport JWT

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var createError = require('http-errors');
 var express = require('express');
+var config = require('./config');
 var path = require('path');
 var passport = require('passport');
 var authenticate = require('./authenticate');
@@ -19,7 +20,7 @@ const mongoose = require('mongoose');
 
 const Dishes = require('./models/dishes');
 
-const url = 'mongodb://localhost:27017/conFusion';
+const url = config.mongoUrl;
 const connect = mongoose.connect(url);
 
 connect.then((db) => {
@@ -35,34 +36,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(session({
-	name: 'session-id',
-	secret: '12345-67890-09876-54321',
-	saveUninitialized: false,
-	resave: false,
-	store: new FileStore()
-}));
-
 app.use(passport.initialize());
 app.use(passport.session());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-function auth (req, res, next) {
-	console.log(req.user);
-
-	if (!req.user) {
-		var err = new Error('You are not authenticated!');
-		err.status = 403;
-		next(err);
-	}
-	else {
-		next();
-	}
-}
-
-app.use(auth);
 app.use('/dishes', dishRouter);
 
 // catch 404 and forward to error handler
